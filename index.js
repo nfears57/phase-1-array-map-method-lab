407c711c6fbdc995adad769d281b60ca742bb587
Nick <nfears5798@gmail.com>
2022-09-30 14:01:39 -0400

    commit tree 3430470f212fb78e343dcd18200b66f9214d265c

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ 
+  return tutorials.map((line) => {
+    const first = line.split(" ")
+    const firstCapital = first.map(
+      capital => capital.charAt(0).toUpperCase() + capital.slice(1))
+      const output = firstCapital.join(" ")
+      return output
+  } )
+  
+
 }
+
+
+// const arr = str.split(" ")
+// for(const i = 0; i < arr.length; i++) {
+//   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+// }
+// const arr2 = arr.join(" ")
+// console.log(arr2)
